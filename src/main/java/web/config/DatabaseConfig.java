70432880eb6d2f7068abfcb8548c7aa6183d6791
sebeb-siz <sebebsiz93@gmail.com>
2023-10-26 02:06:34 +0300

    First commit

---
+++ src/main/java/web/config/DatabaseConfig.java
@@ -0,0 +1,69 @@
+package web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@ComponentScan("web")
+@EnableTransactionManagement
+public class DatabaseConfig {
+    private Environment env;
+
+    @Autowired
+    public DatabaseConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("db.driver")));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
+    }
+    @Bean
+    public Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.CharSet", env.getProperty("hibernate.CharSet"));
+        properties.put("hibernate.characterEncoding", env.getProperty("hibernate.characterEncoding"));
+        properties.put("hibernate.useUnicode", env.getProperty("hibernate.useUnicode"));
+        return properties;
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean getEntityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(getDataSource());
+
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factoryBean.setJpaProperties(getHibernateProperties());
+        factoryBean.setPackagesToScan("web/model");
+        return factoryBean;
+    }
+    @Bean
+    public PlatformTransactionManager getTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(getEntityManagerFactory().getObject());
+        return transactionManager;
+    }
+}
---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,16 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> getAllUsers();
+
+    void addUser(User user);
+
+    void deleteUser(Long id);
+
+    void updateUser(User user);
+    User getUserById(Long id);
+}
